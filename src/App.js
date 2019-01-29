6dd9c782bccb64a45291005994f3170245bad988
Solomonchik <viktorzimmermann11@web.de>
2019-01-29 19:08:08 +0100

    created some folders and files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import Layout from './hoc/Layout/Layout'
+import {Route, Switch} from "react-router-dom";
+import Auth from './containers/Auth/Auth'
+import QuizCreator from './containers/QuizCreator/QuizCreator'
+import QuizList from './containers/QuizList/QuizList'
+
+import Quiz from './containers/Quiz/Quiz'
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Switch>
+          <Route path="/auth" component={Auth}/>
+          <Route path="/quiz-creator" component={QuizCreator}/>
+          <Route path="/quiz/:id" component={Quiz}/>
+          <Route path="/" component={QuizList}/>
+        </Switch>
+      </Layout>
+    )
+  }
+}
+
+export default App
