e1689fc10f7dd425d20d5be09a0d4dce18e98cb3
Shteyn <tarna83@mail.ru>
2019-02-04 18:05:19 +0100

    hrk

--- server.js
+++ server.js
@@ -25,12 +25,11 @@ mongoose //-->Connect to MongoDB
 app.use("/api/films", films); // --> Any request that goes to api/films/* goes to the ./routes/api/films
 
 //Serve static assets if we in prod
+// Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  // Exprees will serve up production assets
+  // Set static folder
   app.use(express.static("client/build"));
 
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
