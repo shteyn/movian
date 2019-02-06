d00c74207da6613403bb0f2f083518fd0b6eb48c
Solomonchik <viktorzimmermann11@web.de>
2019-02-06 09:21:07 +0100

    modified some files

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,55 +1,55 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FilmModal from "./FilmModel";
 
 import {
-  Container,
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink
+    Container,
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
 } from "reactstrap";
 
 export default class Example extends Component {
-  state = {
-    isOpen: false
-  };
-  toggle = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  };
+    state = {
+        isOpen: false
+    };
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    };
 
-  render() {
-    return (
-      <Container className="mb-5">
-        <Navbar color="light" light expand="md" className="mb-5">
-          <NavbarBrand href="/">Movian</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink>
-                  <FilmModal />
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="api/actors/">Actors</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="api/genre/">Genre</NavLink>
-              </NavItem>
+    render() {
+        return (
+            <Container className="AppNavbarContainer">
+                <Navbar light expand="md" className="md-5">
+                    <NavbarBrand href="/" style={{color: "white"}}>Movian</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}/>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink>
+                                    <FilmModal style={{color: "white"}}/>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="api/actors/" style={{color: "white"}}>Movie</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="api/genre/" style={{color: "white"}}>Genre</NavLink>
+                            </NavItem>
 
-              <NavItem>
-                <NavLink href="api/login/">Login</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </Container>
-    );
-  }
+                            <NavItem>
+                                <NavLink href="api/login/" style={{color: "white"}}>Login</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </Container>
+        );
+    }
 }
--- client/src/components/FilmsList.js
+++ client/src/components/FilmsList.js
@@ -1,63 +1,80 @@
-import React, { Component } from "react";
-import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
-import { connect } from "react-redux";
-import { getFilms, deleteFilm } from "../actions/filmAction";
+import React, {Component} from "react";
+import {Container, ListGroup, ListGroupItem, Button} from "reactstrap";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
+import {connect} from "react-redux";
+import {getFilms, deleteFilm} from "../actions/filmAction";
 import propTypes from "prop-types";
 
+// import CardComponent from './UI/CardComponent'
+
 class FilmsList extends Component {
-  componentDidMount() {
-    this.props.getFilms();
-  }
+    componentDidMount() {
+        this.props.getFilms();
+    }
 
-  /*****DELETE FILM****
-   **** FIRST STEP
-   ********************/
-  //sends id to filmAction.js
-  deleteFilmBtn = id => {
-    this.props.deleteFilm(id);
-  };
+    /*****DELETE FILM****
+     **** FIRST STEP
+     ********************/
+        //sends id to filmAction.js
+    deleteFilmBtn = id => {
+        this.props.deleteFilm(id);
+    };
 
-  render() {
-    const { films } = this.props.film;
-    return (
-      <Container>
-        <ListGroup>
-          <TransitionGroup className="films-list">
-            {films.map(({ _id, name, year, description, actor, image }) => (
-              <CSSTransition key={_id} timeout={500} classNames="fade">
-                <ListGroupItem className="mt-4">
-                  <Button
-                    className="remove-btn mr-3 btn-danger"
-                    onClick={this.deleteFilmBtn.bind(this, _id)}
-                  >
-                    &times;
-                  </Button>
-                  <div>{name}</div>
-                  <div>{year}</div>
-                  <div>{description}</div>
-                  <div>{actor}</div>
-                  <div>
-                    <img src={image} alt="" />
-                  </div>
-                </ListGroupItem>
-              </CSSTransition>
-            ))}
-          </TransitionGroup>
-        </ListGroup>
-      </Container>
-    );
-  }
+    render() {
+        const {films} = this.props.film;
+        return (
+            <Container className="FilmsListContainer">
+                <ListGroup>
+                    <TransitionGroup className="films-list">
+                        {films.map(({_id, name, year, description, actor, image}) => (
+                            <CSSTransition key={_id} timeout={500} classNames="fade">
+                                <ListGroupItem className="ListGroupItem">
+                                    <div className="imgDiv">
+                                        <img src={image} alt="" width="318" height="180"/>
+                                    </div>
+                                    <div><span>Name:</span> {name}</div>
+                                    <div><span>Year:</span> {year}</div>
+                                    <div><span>Description:</span> {description}</div>
+                                    <div><span>Actor:</span> {actor}</div>
+
+                                    <Button
+                                        className="remove-btn mr-3 btn-danger"
+                                        onClick={this.deleteFilmBtn.bind(this, _id)}
+                                    >
+                                        <span>remove</span>
+                                    </Button>
+
+                                    <Button
+                                        className="remove-btn mr-3 btn-danger"
+                                        // onClick={this.deleteFilmBtn.bind(this, _id)}
+                                    >
+                                        <span>settings</span>
+                                    </Button>
+
+                                    <Button
+                                        className="remove-btn mr-3 btn-danger"
+                                        // onClick={this.deleteFilmBtn.bind(this, _id)}
+                                    >
+                                        <span>для долбоёбов</span>
+                                    </Button>
+                                </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
+            </Container>
+        );
+    }
 }
 
 FilmsList.propTypes = {
-  getFilms: propTypes.func.isRequired,
-  film: propTypes.object.isRequired
+    getFilms: propTypes.func.isRequired,
+    film: propTypes.object.isRequired
 };
 const mapStateToProps = state => ({
-  film: state.film
+    film: state.film
 });
 export default connect(
-  mapStateToProps,
-  { getFilms, deleteFilm }
+    mapStateToProps,
+    {getFilms, deleteFilm}
 )(FilmsList);
