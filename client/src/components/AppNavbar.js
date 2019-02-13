508d078f66d29575a5ac19412f70d93724e4e4a4
moe666 <bobcarter@hotmail.de>
2019-02-13 14:00:40 +0100

    LogIn Sign Form with frontend validation

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FilmModal from "./FilmModel";
+import LogIn from "./LogIn";
+import SignUp from "./SignUp";
 
 import {
   Container,
@@ -31,11 +33,13 @@ export default class Example extends Component {
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
+
               <NavItem>
                 <NavLink>
                   <FilmModal />
                 </NavLink>
               </NavItem>
+
               <NavItem>
                 <NavLink href="api/actors/">Actors</NavLink>
               </NavItem>
@@ -44,7 +48,15 @@ export default class Example extends Component {
               </NavItem>
 
               <NavItem>
-                <NavLink href="api/login/">Login</NavLink>
+                <NavLink>
+                  <LogIn />
+                </NavLink>
+              </NavItem>
+
+              <NavItem>
+                <NavLink>
+                  <SignUp />
+                </NavLink>
               </NavItem>
             </Nav>
           </Collapse>
